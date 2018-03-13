7b71524a63fc1c91c8becd4d4f3f049465499d14
Vitek - ASUS <Vitek - ASUS@DESKTOP-2TGQGTC>
2018-03-13 15:24:44 +0100

    add Interface

---
+++ src/balicek/IInformace.java
@@ -0,0 +1,50 @@
+package balicek;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Interface slouží ke třídě Informace
+ * 
+ * @author Vít Vágner
+ * @version 1.0
+ */
+
+public interface IInformace {
+	
+	/**
+	 * Přiřadí k dokumenu cestu
+	 * 
+	 * @param path		textový řetezec cesta k dokumentu
+	 * @throws FileNotFoundException		když se dokument nenajde
+	 */
+	void setPathToFile(String path) throws FileNotFoundException;
+	
+	/**
+	 * Vrací počet všech řádků, co jsou ve dokumentu.
+	 * 
+	 * @return		int
+	 */
+	int getNumberOfLines();
+	
+	/**
+	 * Vrací počet všech slov, které jsou v dokumentu.
+	 * 
+	 * @return		int
+	 */
+	int getNumberOfWords();
+	
+	/**
+	 * Vrací počet všech no-whitespace znaků, které jsou v dokumentu.
+	 * 
+	 * @return		int
+	 */
+	int getNumberOfNoneWhite();
+	
+	/**
+	 * Vrací počet všech znaků ve daném dokumentu.
+	 * 
+	 * @return		int
+	 */
+	int getNumberOfAllChar();
+
+}
